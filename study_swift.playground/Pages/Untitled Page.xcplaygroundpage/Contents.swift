58fa9ab9e48ab5f84cc2d582e24c4f166455ce0a
jongdroid <qkr7627@gmail.com>
2022-01-05 17:41:43 +0900

    문자열 보간법 연습

--- study_swift.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ study_swift.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// 변수 및 문자열 보간법 연습 
+
+var statusSwitch : Bool = true
+print(statusSwitch)
+
+if statusSwitch == true
+{
+    print("your switch status is [\(statusSwitch)] \nare you want OFF?")
+}
+else
+{
+    print("your switch status is OFF")
+}
