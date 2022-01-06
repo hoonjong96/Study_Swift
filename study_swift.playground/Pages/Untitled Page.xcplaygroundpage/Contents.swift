3887f428a47ce79096b4be818f657c1f2958fb25
jongdroid <qkr7627@gmail.com>
2022-01-06 13:44:25 +0900

    1. tistory

--- study_swift.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ study_swift.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -6,3 +6,7 @@ let chooseLunchArray : [String] = ["햄버거", "탕수육", "치킨"]
 print("오늘 점심으로 무엇을 드시겠습니까? [\(chooseLunchArray[1])]")
 
 
+var myintArray : [Int] = [1,2,3,4,5]
+myintArray.append(6)
+myintArray.remove(at: 5)
+print(myintArray)
