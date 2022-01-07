3f4b601e2df498460fb77e59d17609738edd5ce6
jongdroid <qkr7627@gmail.com>
2022-01-07 16:30:42 +0900

    Tuple

--- study_swift.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
+++ study_swift.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -2,18 +2,21 @@ import UIKit
 
 //Tuple
 
-var myTuple : (String, Int, Int) =  ("Da", 158, 25)
-print("그녀의 이름은 : \(myTuple.0) 입니다")
-print(myTuple.1)
-myTuple.1 = 55
-print(myTuple.1)
+//var girlInfo : (String, Int, Int) =  ("may", 158, 25)
+//print("그녀의 이름은 : \(girlInfo.0) 입니다")
+//print(girlInfo.1)
+//girlInfo.2 = 26
+//print(girlInfo.2)
 
-var tupleName : (name : String, age : Int) = ("인태", 28)
-print(tupleName.name, "오빠 는", tupleName.age, "살 입니다")
 
-typealias PersonTuple = (name : String, age : Int)
-var myTyple : PersonTuple = ("종드", 22)
-var myTuple2 : PersonTuple = ("홍잉", 33)
+// Tuple2
+//var userData : (name : String, age : Int) = ("홍종", 28)
+//print(userData.name, userData.age, "살")
 
-print("학생의 이름은 \(myTyple.name), 학생의 나이는 \(myTyple.age) 입니다")
+
+// Tuple3
+typealias UserTuple = (name : String, age : Int, hobby : String)
+var user1 : UserTuple = ("종드", 22, "코딩")
+var user2 : UserTuple = ("Da", 33, "take a pictures")
+print("user Information : name : [\(user2.name)], age : [\(user1.age)], hobby :  [\(user2.hobby)]  ")
 
