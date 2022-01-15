51aabd28bce4e20e9b0436ff00a206acbadcd7df
jongdroid <qkr7627@gmail.com>
2022-01-15 20:09:01 +0900

    기본연산자 - 형변환

--- study_swift.playground/Pages/기본 연산자.xcplaygroundpage/Contents.swift
+++ study_swift.playground/Pages/기본 연산자.xcplaygroundpage/Contents.swift
@@ -6,9 +6,20 @@ let b = 30
 
 let c = a + b
 
+//let num1: Int = 20
+//let num2: Double = 30
+//
+//let d = num1 / Int(num2)
+//
+//let ok = num1 + Int(num2)
+
+
 let num1: Int = 20
-let num2: Double = 30
+let num2: Double = 9.999
 
-let d = num1 / Int(num2)
+let sum = num1 + Int(num2)
+print("형 변환 적용을 하지 않았어요:",num2)
+print( "Double 형 변수 형변환을 적용하였습니다:",Int(num2) )
+print(sum)
 
-let ok = num1 + Int(num2)
+
